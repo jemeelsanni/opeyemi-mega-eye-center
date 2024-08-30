191120f5c5e0ee1056bc29aaf18d110d68bfa1ae
jemeelsanni <jemeelsanni@gmail.com>
2024-08-30 10:19:27 +0100

    done

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-quill'], // Exclude react-quill from the bundle
+    },
+  },
 })
