3bd0f6a968a50397bebc4cb42a38c73403aea1d6
jemeelsanni <jemeelsanni@gmail.com>
2024-10-31 21:33:44 +0100

    feat: netlify

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 import Navbar from "../../layout/navbar";
 import Footer from "../../layout/footer";
 import Header from "../../layout/header";
 
 const Contact: React.FC = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    fullName: "",
+    phoneNumber: "",
+    message: "",
+  });
+
+  const [feedback, setFeedback] = useState<string>("");
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Set up your EmailJS configuration
+    const serviceId = "service_3manlb8";
+    const templateId = "template_aexwzmw";
+    const userId = "97ruIVm8heto_ih_y";
+
+    emailjs
+      .send(serviceId, templateId, formData, userId)
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        setFeedback("Enquiry sent successfully!");
+        setFormData({ email: "", fullName: "", phoneNumber: "", message: "" });
+      })
+      .catch((error) => {
+        console.error("FAILED...", error);
+        setFeedback("Failed to send enquiry. Please try again.");
+      });
+  };
+
   return (
     <div>
       <Header />
@@ -24,105 +62,69 @@ const Contact: React.FC = () => {
         </div>
       </div>
 
-      {/* Form */}
-      <div className="hero-large-screen">
-        <div className="mx-[408px] my-16">
-          <div className="text-center">
-            <h1 className="text-3xl py-2 font-medium text-center mt-0 mb-2">
-              Get in touch
-            </h1>
-            <p className="text-lg pb-5">
-              Got a question about us? Have some suggestions or just want to say
-              Hi? Just contact us. We are here to assist you.
-            </p>
-          </div>
-          <form className="flex flex-col text-lg gap-8" action="">
-            <div className="flex flex-col">
-              <label htmlFor="email">Email</label>
-              <input
-                id="email"
-                className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
-                type="email"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="fullName">Full Name</label>
-              <input
-                id="fullName"
-                className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
-                type="text"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="phoneNumber">Phone Number</label>
-              <input
-                id="phoneNumber"
-                className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
-                type="tel"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="message">How can we help you?</label>
-              <textarea
-                id="message"
-                className="outline-none border-[#FFA500] h-48 border-[1px] bg-gray p-2"
-              />
-            </div>
-            <button type="submit" className="bg-[#FFA500] p-4 text-lg">
-              Submit Enquiry
-            </button>
-          </form>
+      <div className="hero-large-screen mx-[408px] my-16">
+        <div className="text-center">
+          <h1 className="text-3xl py-2 font-medium text-center mt-0 mb-2">
+            Get in touch
+          </h1>
+          <p className="text-lg pb-5">
+            Got a question about us? Have some suggestions or just want to say
+            Hi? Just contact us. We are here to assist you.
+          </p>
         </div>
-      </div>
-
-      <div className="hero-small-screen">
-        <div className="mx-6 my-6">
-          <div className="text-center">
-            <h1 className="text-3xl py-2 font-medium text-center mt-0 mb-2">
-              Get in touch
-            </h1>
-            <p className="text-lg pb-5">
-              Got a question about us? Have some suggestions or just want to say
-              Hi? Just contact us. We are here to assist you.
-            </p>
+        <form onSubmit={handleSubmit} className="flex flex-col text-lg gap-8">
+          <div className="flex flex-col">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
           </div>
-          <form className="flex flex-col text-lg gap-8" action="">
-            <div className="flex flex-col">
-              <label htmlFor="emailSmall">Email</label>
-              <input
-                id="emailSmall"
-                className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
-                type="email"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="fullNameSmall">Full Name</label>
-              <input
-                id="fullNameSmall"
-                className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
-                type="text"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="phoneNumberSmall">Phone Number</label>
-              <input
-                id="phoneNumberSmall"
-                className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
-                type="tel"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="messageSmall">How can we help you?</label>
-              <textarea
-                id="messageSmall"
-                className="outline-none border-[#FFA500] h-48 border-[1px] bg-gray p-2"
-              />
-            </div>
-            <button type="submit" className="bg-[#FFA500] p-4 text-lg">
-              Submit Enquiry
-            </button>
-          </form>
-        </div>
+          <div className="flex flex-col">
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              id="fullName"
+              className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
+              type="text"
+              value={formData.fullName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input
+              id="phoneNumber"
+              className="outline-none border-[#FFA500] border-[1px] bg-gray p-2"
+              type="tel"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="message">How can we help you?</label>
+            <textarea
+              id="message"
+              className="outline-none border-[#FFA500] h-48 border-[1px] bg-gray p-2"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="bg-[#FFA500] p-4 text-lg">
+            Submit Enquiry
+          </button>
+        </form>
+        {feedback && (
+          <div className="mt-4 text-center">
+            <p>{feedback}</p>
+          </div>
+        )}
       </div>
 
       <Footer />
